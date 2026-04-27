73d9891580610911f98ec4119f59cd257e74b08e
giorgi-jinikashvili <geo135383@gmail.com>
2026-04-27 17:45:26 +0400

    commit

---
+++ level 03/classwork/homework.py
@@ -0,0 +1,12 @@
+
+m=int(input("enter x: "))
+c=int(input("enter y: "))
+
+print(m < c)
+print(m >= c)
+print(m > c)
+print(m == c)
+print(m <= c)
+print(m != c)
+print(m > c)
+print(m < c)
---
+++ level 03/homework/homework.py
@@ -0,0 +1,43 @@
+# name = input("enter your name: ")
+# print("hello" + " " + name)
+
+# number =int(input("enter number: "))
+# print(number * 2)
+
+x =int(input("enter x: "))
+y =int(input("enter y: "))
+
+print(y + x)
+print(x - y)
+print(x * y)
+print(x ** y)
+print(x / y)
+print(x // y)
+print(x % y)
+
+# დიალოგი 2 ადამიანს შორის
+adamiani1 = input("enter your name: ")
+adamiani2 = input("enter your name: ")
+
+fraza1 = input("enter first phraze: ")
+fraza2 = input("enter second phraze: ")
+fraza3 = input("enter third phraze: ")
+fraza4 = input("enter fourth phraze: ")
+fraza5 = input("enter fifth phraze: ")
+fraza6 = input("enter sixth phraze: ")
+fraza7 = input("enter seventh phraze: ")
+print(" ")
+print("dialogi daiwyo")
+print(" ")
+print(f"{adamiani1}: {fraza1}")
+print(f"{adamiani2}: {fraza2}")
+print(f"{adamiani1}: {fraza3}")
+print(f"{adamiani2}: {fraza4}")
+print(f"{adamiani1}: {fraza5}")
+print(f"{adamiani2}: {fraza6}")
+print(f"{adamiani1}: {fraza7}")
+print(" ")
+print("dialogi damtavrda")
+
+#  რა არის input? input-ი გვეხმარება კომპიუტერში ინფორმაციის შემოტანაში ანუ input არის ინფორმაცია რომელიც კომპიუტერში შედის
+#  რა არის int()-str()? int() არის ციფრები ხოლო str() არის სიტყვა-წინადადება და ამათ ერთმანეთს ვერ დავუმატებთ 
