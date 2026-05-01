79ea4e20a6f4400725e9f667e93b796bcd522fdb
giorgi-jinikashvili <geo135383@gmail.com>
2026-05-01 17:10:06 +0400

    commit

---
+++ level 05/classwork/mine.py
@@ -0,0 +1,10 @@
+for i in range(2, 52):
+    print(i)
+
+goa = "goal oriented academy"
+for i in goa:
+    print(goa)
+
+
+for i in range(10, 72):
+    print(i * 3)
---
+++ level 05/homework/mine.py
@@ -0,0 +1,14 @@
+for i in range(20):
+    print(i)
+
+for i in range(0, 50, 2):
+    print(i)
+
+n = int(input("enter n: "))
+for i in range(0, n):
+    print(i)
+
+
+goa = input("what you want to print: ")
+for i in goa:
+    print(i) 
