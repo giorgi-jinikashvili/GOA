e8f05b3bf0f9c3a12884a7d3575fa8d6f3f766b5
giorgi-jinikashvili <geo135383@gmail.com>
2026-05-04 18:26:08 +0400

    commit

---
+++ level 06/class/mine.py
@@ -0,0 +1,11 @@
+usernme = "goal oriented academy"
+password = "goal oriented academy"
+
+guess1 = input("enter username: ")
+guess2 = input("enter password: ")
+
+while usernme != guess1 and password != guess2:
+ guess1 = input("enter username: ")
+ guess2 = input("enter password: ")
+
+print("Acces Granted")
---
+++ level 06/home/main.py
@@ -0,0 +1,31 @@
+# davaleba 2
+
+word = input("enter word: ")
+for i in range(15):
+    print(word)
+
+#davaleba 3
+x = 11
+y = int(input("enter x: "))
+while y!=x:
+    print("try egein")
+    y = int(input("enter x: "))
+    print("acces")
+
+#davaleba 4
+
+x = int(input("enter x: "))
+while x < 150:
+    print(x)
+    x *= 2
+
+# davaleba 5
+x = int(input("enter x: "))
+y = int(input("enter y: "))
+print(x + y)
+f = int(input("enter f: "))
+print(x + y + f)
+c = int(input("enter c: "))
+print(x + y + f + c)
+a = int(input("enter a: "))
+print(x + y + f + c + a)
